48261a9696ba4c6e2cc6a30d91bbe9bdd02822bb
Damien Lawford <killerdamo@hotmail.co.uk>
2013-10-26 18:19:42 +0100

    small fix

--- gframe/image_manager.cpp
+++ gframe/image_manager.cpp
@@ -66,7 +66,7 @@ irr::video::ITexture* ImageManager::GetTexture(int code) {
 		irr::video::ITexture* img = driver->getTexture(file);
 		if(img == NULL) {
 			tMap[code] = NULL;
-			return GetTextureThumb(code);
+			return tUnknown;
 		} else {
 			tMap[code] = img;
 			return img;
@@ -75,7 +75,7 @@ irr::video::ITexture* ImageManager::GetTexture(int code) {
 	if(tit->second)
 		return tit->second;
 	else
-		return GetTextureThumb(code);
+		return tUnknown;
 }
 irr::video::ITexture* ImageManager::GetTextureThumb(int code) {
 	if(code == 0)
