51b2ba6ecc4eee06f0d56a77d17ca1e06038e1fd
sidschingis <sijing@gmx.net>
2014-07-27 21:04:29 +0200

    typo

--- gframe/event_handler.cpp
+++ gframe/event_handler.cpp
@@ -745,7 +745,6 @@ bool ClientField::OnEvent(const irr::SEvent& event) {
 					} else{
 						myswprintf(formatBuffer, L"%ls[%d]", dataManager.FormatLocation(selectable_cards[i + pos]->location,
 							selectable_cards[i + pos]->sequence), selectable_cards[i + pos]->sequence + 1);
-					           selectable_cards[i + pos]->sequence + 1);
 					}
 					mainGame->stCardPos[i]->setText(formatBuffer);
 					if(selectable_cards[i + pos]->is_selected)
