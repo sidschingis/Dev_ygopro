00b2fd8fcf42e33e5b6e7b62fb042a0683d92ced
sidschingis <sijing@gmx.net>
2016-06-05 13:30:19 +0200

    defence > defense

--- gframe/duelclient.cpp
+++ gframe/duelclient.cpp
@@ -2853,8 +2853,8 @@ int DuelClient::ClientAnalyze(char * msg, unsigned int len) {
 			pcard->attack = aatk;
 			myswprintf(pcard->atkstring, L"%d", aatk);
 		}
-		if(adef != pcard->defence) {
-			pcard->defence = adef;
+		if(adef != pcard->defense) {
+			pcard->defense = adef;
 			myswprintf(pcard->defstring, L"%d", adef);
 		}
 		if(ld) {
@@ -2863,8 +2863,8 @@ int DuelClient::ClientAnalyze(char * msg, unsigned int len) {
 				pcard->attack = datk;
 				myswprintf(pcard->atkstring, L"%d", datk);
 			}
-			if(ddef != pcard->defence) {
-				pcard->defence = ddef;
+			if(ddef != pcard->defense) {
+				pcard->defense = ddef;
 				myswprintf(pcard->defstring, L"%d", ddef);
 			}
 		}
