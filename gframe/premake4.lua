e9ed4e46de1e20b8e61d9e263adb0f914f6cac45
Damien Lawford <DJLawford@virginmedia.com>
2016-06-24 14:28:17 +0100

    SFML Update

--- gframe/premake4.lua
+++ gframe/premake4.lua
@@ -5,7 +5,7 @@ project "ygopro"
 
     files { "**.cpp", "**.cc", "**.c", "**.h" }
     excludes "lzma/**"
-    includedirs { "../ocgcore", "../irrKlang-1.4.0/include", "../SFML-2.1/include" }
+    includedirs { "../ocgcore", "../irrKlang-1.4.0/include", "../SFML/include" }
     links { "ocgcore", "clzma", "Irrlicht", "freetype", "sqlite3", "lua", "event", "sfml-network", "sfml-system", "irrKlang" }
 
     configuration "windows"
@@ -14,7 +14,7 @@ project "ygopro"
         includedirs { "../irrlicht/include", "../freetype/include", "../event/include", "../sqlite3" }
         links { "opengl32", "ws2_32", "winmm", "gdi32", "kernel32", "user32", "imm32" }
     configuration {"windows", "vs*"}
-        libdirs { "../irrKlang-1.4.0/lib/Win32-visualStudio", "../SFML-2.1/lib" }
+        libdirs { "../irrKlang-1.4.0/lib/Win32-visualStudio", "../SFML/lib" }
     configuration {"windows", "not vs*"}
         includedirs { "/mingw/include/irrlicht", "/mingw/include/freetype2" }
     configuration "not vs*"
